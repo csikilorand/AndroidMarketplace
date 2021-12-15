2d84d03ca4ca2db18129fe380e539a1f1db6e202
Csiki Lorand <csiki.lori95@gmail.com>
2021-12-15 20:33:43 +0200

    init commit

---
+++ app/src/main/java/com/example/mymarketplace/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mymarketplace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(applicationContext,"Hello", Toast.LENGTH_LONG).show()
+    }
+}
